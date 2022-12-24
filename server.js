41ed63f54efa7d3ed2e55c42abed584e280c8d09
Glitch (sepia-dandy-yogurt) <none>
2022-12-24 15:57:44 +0000

    🎩🙉 Checkpoint ./server.js:52460468/594

--- server.js
+++ server.js
@@ -6,6 +6,17 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
 
+/**
+* Express needs to know which template engine you are using. 
+* 1) Use the set method to assign pug as the view engine property's value
+* 2) After that, add another set method that sets the views property of your app to point to the ./views/pug directory. 
+* This tells Express to render all views relative to that directory.
+* 3) After that, add another set method that sets the views property of your app to point to the ./views/pug directory. 
+* This tells Express to render all views relative to that directory.
+**/
+app.set('view engine', 'pug');
+app.set('views', )
+
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
